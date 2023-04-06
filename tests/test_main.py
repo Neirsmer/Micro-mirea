8047c07e4a80c6d29206bdc714ab4e919b90a739
Neirsmer <rin1702@gmail.com>
2023-04-06 14:39:17 +0300

    Nya5

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import requests
 api = 'http://localhost:7000'
 def test_health():
     response =requests.get(f'{api}/health')
-    assert response.status_code == 200
+    assert response.status_code == 400
 def test_bye():
     response =requests.get(f'{api}/1_nya')
     assert response.status_code == 200
